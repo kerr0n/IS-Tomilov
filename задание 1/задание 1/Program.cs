e94b716b9b96cdb12c797b7a06d5e60b53963b8e
Ноут <tomilovstas1@gmail.com>
2026-09-03 15:40:52 +0700

    задание 2.1.1

---
+++ задание 1/задание 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Задание_1;
+
+List<Temperature> temperatures = new List<Temperature>
+{
+    new Temperature(new DateOnly(2026, 1, 1), "Место 1", -5.0),
+    new Temperature(new DateOnly(2026, 1, 2), "Место 2", -3.5),
+    new Temperature(new DateOnly(2026, 1, 3), "Место 3", -2.0),
+    new Temperature(new DateOnly(2026, 1, 4), "Место 4", -4.5),
+    new Temperature(new DateOnly(2026, 1, 5), "Место 5", -6.0)
+};
+foreach (Temperature temperature in temperatures)
+{
+    Console.WriteLine(temperature.GetAll());
+}
---
+++ задание 1/задание 1/Temperature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Задание_1
+{
+    public class Temperature
+    {
+        private DateOnly _date;
+        private string _place;
+        private double _value;
+        public Temperature(DateOnly date, string place, double value)
+        {
+            _date = date;
+            _place = place;
+            _value = value;
+        }
+        public string GetAll()
+        {
+            return $"Дата: {_date.ToString("dd.MM.yyyy")}, место измерения: \"{_place}\", значение: {_value}";
+        }
+    }
+}
